c78b37e3e66517347fdb6d1727695ee65d3cd910
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-03-16 12:10:36 -0400

    toml changes for mkdbook v0.5.2
    removed highlight.js copy, since we don't actually have fortran snippets
    Should probably make a theme that has Fortran if needed

--- buildit.sh
+++ buildit.sh
@@ -4,4 +4,4 @@ mdbook build -d docs
 
 # Add Fortran syntax highlighting
 # Note this breaks the rust # hidding avaiable in mdbooks
-cp highlight.min.js docs/highlight.js
+#cp highlight.min.js docs/highlight.js
