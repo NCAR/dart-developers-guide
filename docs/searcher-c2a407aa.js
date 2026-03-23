36426a0c885fa1c28c3afe6174b856bf8c22243c
Helen Kershaw <20047007+hkershaw-brown@users.noreply.github.com>
2026-03-23 14:18:24 -0400

    latest docs build

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-589e628a.js',
+                path_to_root + 'searchindex-5afb9df3.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
